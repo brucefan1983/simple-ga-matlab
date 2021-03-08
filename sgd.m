c3f2b58a529362457bcc3742edafc9452fc88b3d
Zheyong Fan <brucenju@gmail.com>
2021-03-09 05:57:13 +0800

    nesterov

--- sgd.m
+++ sgd.m
@@ -1,39 +1,30 @@
 clear; %close all;
-
-use_batch = false;
-
-% the training data set
 x0 = 1 : 0.01 : 3;
 N_samples = length(x0);
 U0 = 10./x0.^12 - 10./x0.^6 + rand(1, N_samples) * 0.1;
 U = zeros(1, N_samples);
-
-% initialize the NN parameters
 N_neurons = 10; % 1-10-10-1 NN
-u = (rand(N_neurons, 1) - 0.5)*5;
-v = (rand(N_neurons, N_neurons) - 0.5)*5;
-w = (rand(1, N_neurons) - 0.5)*5;
-a = (rand(N_neurons, 1) - 0.5)*5;
-b = (rand(N_neurons, 1) - 0.5)*5;
-c = (rand - 0.5)*5;
-
-learning_rate = 0.01; % should be relatively large for batch GD
-if use_batch
-    learning_rate = 0.1;
-end
-
-N_steps = 100000;
+N_neurons2 = N_neurons * N_neurons;
+N_para = N_neurons * (N_neurons + 4) + 1;
+para = (rand(N_para, 1) - 0.5) * 5;
+para_tmp = para;
+momentum = zeros(N_para, 1);
+beta = 0.9;
+learning_rate = 0.01;
+N_steps = 10000;
 rmse_U = zeros(N_steps, 1);
 for step = 1 : N_steps
-    % derivatives of the error wrt weights and biases
-    dEdw = zeros(1, N_neurons);
-    dEdv = zeros(N_neurons, N_neurons);
-    dEdu = zeros(N_neurons, 1);
-    dEdc = 0;
-    dEdb = zeros(N_neurons, 1);
-    dEda = zeros(N_neurons, 1);
+    para_d = zeros(N_para, 1);
+    para_tmp = para + beta * momentum;
     for n_sample = 1 : N_samples
-        % propagate the NN forward
+        % get the parameters:
+        u = para_tmp(1:N_neurons); offset = N_neurons;
+        v = reshape(para_tmp(offset+1 : offset + N_neurons*N_neurons), N_neurons, N_neurons); offset = offset + N_neurons*N_neurons;
+        w = para_tmp(offset+1 : offset + N_neurons).'; offset = offset + N_neurons;
+        a = para_tmp(offset+1 : offset + N_neurons); offset = offset + N_neurons;
+        b = para_tmp(offset+1 : offset + N_neurons);
+        c = para_tmp(end);
+        % propagate the NN:
         y = tanh(u * x0(n_sample) - a);
         yd = 1 - y .* y;
         z = tanh(v * y - b);
@@ -41,54 +32,27 @@ for step = 1 : N_steps
         U(n_sample) = w * z - c;
         % calculate the derivatives
         deltaU = U(n_sample) - U0(n_sample);
-        dEdw0 = deltaU * z.';
-        dEdc0 = - deltaU;
-        dEdv0 = deltaU * (w.' .* zd) * y.';
-        dEdb0 = - deltaU * w.' .* zd;
-        dEdu0 = deltaU * x0(n_sample) * yd .* (v.' * zd .* w.');
-        dEda0 = - deltaU * yd .* (v.' * zd .* w.');
-        if use_batch
-            dEdw = dEdw + dEdw0;
-            dEdc = dEdc + dEdc0;
-            dEdv = dEdv + dEdv0;
-            dEdb = dEdb + dEdb0;
-            dEdu = dEdu + dEdu0;
-            dEda = dEda + dEda0;
-        else
-            u = u - learning_rate * dEdu0;
-            v = v - learning_rate * dEdv0;
-            w = w - learning_rate * dEdw0;
-            a = a - learning_rate * dEda0;
-            b = b - learning_rate * dEdb0;
-            c = c - learning_rate * dEdc0;
-        end
+        para_d1 = zeros(N_para, 1);
+        para_d1(1:N_neurons) = deltaU * x0(n_sample) * yd .* (v.' * zd .* w.'); offset = N_neurons;
+        para_d1(offset+1 : offset + N_neurons*N_neurons) = reshape(deltaU * (w.' .* zd) * y.', N_neurons*N_neurons, 1); offset = offset + N_neurons*N_neurons;
+        para_d1(offset+1 : offset + N_neurons) = deltaU * z.'; offset = offset + N_neurons;
+        para_d1(offset+1 : offset + N_neurons) = - deltaU * yd .* (v.' * zd .* w.'); offset = offset + N_neurons;
+        para_d1(offset+1 : offset + N_neurons) = - deltaU * w.' .* zd;
+        para_d1(end) = - deltaU;
+        para_d = para_d + para_d1;
     end
-    if use_batch
-        eta = learning_rate  / N_samples;
-        u = u - eta * dEdu;
-        v = v - eta * dEdv;
-        w = w - eta * dEdw;
-        a = a - eta * dEda;
-        b = b - eta * dEdb;
-        c = c - eta * dEdc;
-    end
-    
+    momentum = beta * momentum - (learning_rate / N_samples) * para_d;
+    para = para + momentum;
     rmse_U(step) = sqrt(mean((U - U0).^2));
     if mod(step, 1000) == 0
         disp(['Step = ', num2str(step), ', best fitness = ', num2str(rmse_U(step))]);
     end
 end
 
+figure;semilogy(1:N_steps, rmse_U, '-');
+figure;plot(para);
 figure;
-semilogy(1:N_steps, rmse_U, '-');
-
-figure;
-plot(u); hold on;plot(v);plot(w);plot(a);plot(b);plot(c);
-
-figure;
-plot(x0, U0, 'ro');
-hold on;
-plot(x0, U, 'bx');
+plot(x0, U0, 'ro', x0, U, 'bx');
 legend('training data');
 xlabel('x', 'fontsize', 15);
 ylabel('y', 'fontsize', 15);
